59f647aa115866d8f957e34a6b76703865c3283f
JuanCelis2040 <juandiego.06c@gmail.com>
2023-07-13 07:21:23 -0500

    Funcionalidad carrito

--- js/js.js
+++ js/js.js
@@ -1,60 +1,14 @@
-let carrito = [];
-
-function agregarAlCarrito(nombreProducto) {
-    carrito.push(nombreProducto);
-    actualizarCarrito();        
+function incrementarContador() {
+    let contador = document.getElementById("contador");
+    contador.innerHTML = parseInt(contador.innerHTML) + 1;
 }
 
-function eliminarDelCarrito(index) {
-    carrito.splice(index, 1);
-    actualizarCarrito();
-    
-}
-
-function  actualizarCarrito() {
-    let listaCarrito = document.getElementById("items");
-    let contador = document.getElementById("contador")
-
-
-    listaCarrito.innerHTML = "";
-
-    for (let i = 0; i < carrito.length; i++) {
-        let producto = carrito[i];
-        let item = document.createElement("li");
-        item.textContent = producto;
-        listaCarrito.appendChild(item);    
+function disminuirContador() {
+    let contador = document.getElementById("contador");
+    let validar = parseInt(contador.innerHTML);
+    if(validar > 0){
+        contador.innerHTML = validar - 1;
+    }else {
+        alert("No ha seleccionado ningun producto")
     }
-
-    contador.textContent = carrito.length;
-
-
-
-
-
-
-let productoSeparados = "";
-
-
-
-for (let i= 0; i < carrito.length; i++) {
-    productoSeparados += carrito[i];
-    if(i != carrito.length - 1){
-        productoSeparados += ", ";
-    }   
-}
-
-listaCarrito.textContent = productoSeparados;
-
-}
-function comprar() {
-
-    carrito = [];
-    actualizarCarrito();
-    
-}
-
-function eliminar() {
-    carrito = [];
-    eliminarDelCarrito();
-    
 }
